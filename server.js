1800073bdabf57a7d37f329f09a71501c33f79b7
dlauck92 <43586307+dlauck92@users.noreply.github.com>
2019-02-12 18:20:36 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
+// Set Handlebars
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
